27ae9aa200cbf6fc95c0acb56ed0afbaf6f52a5a
Subramanian <Praveen.Subramanian@kla-tencor.com>
2020-12-10 00:32:54 +0530

    added sleep for stability

--- mqtt_utils.py
+++ mqtt_utils.py
@@ -1,13 +1,17 @@
 import paho.mqtt.client as mqtt
 import os
+import time
 #"db9c38ca/TestCaseResults"
 BROKER = os.environ.get('BROKER')
 
 def publish_message(topic,request):
+    print(BROKER)
     client = mqtt.Client()
-    client.connect(BROKER, 1883, 60)
-    client.publish(topic, request)
-    client.disconnect()
+    client.connect(BROKER, 1883, 10)
+    time.sleep(2)
+    if(client.is_connected):
+        client.publish(topic, request)
+        client.disconnect()
 
 def find_N_return(array , key , value ):
     for items in array:
